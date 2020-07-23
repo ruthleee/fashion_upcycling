213e4a51760b5fdf3f1846c28429ea5f6801f330
ruthleee <seojinruthy@gmail.com>
2020-07-23 17:52:43 +0000

    modified

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request
 
 
 # -- Initialization section --
@@ -13,4 +13,4 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template('index.html')
