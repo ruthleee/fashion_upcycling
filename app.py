4ded82cbc49c238b961b95dbe73de8e415e53f7b
ruthleee <seojinruthy@gmail.com>
2020-07-24 23:52:23 +0000

    finally working?

--- app.py
+++ app.py
@@ -14,3 +14,20 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template('index.html')
+@app.route('/upcycleSearch')
+def upcycleSearch():
+    return render_template('upcycleSearch.html')
+
+
+# @app.context_processor
+# def override_url_for():
+#     return dict(url_for=dated_url_for)
+
+# def dated_url_for(endpoint, **values):
+#     if endpoint == 'static':
+#         filename = values.get('filename', None)
+#         if filename:
+#             file_path = os.path.join(app.root_path,
+#                                  endpoint, filename)
+#             values['q'] = int(os.stat(file_path).st_mtime)
+#     return url_for(endpoint, **values)
