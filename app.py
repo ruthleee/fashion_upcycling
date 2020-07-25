9d57db995235d242cfbdc4f28fff92baf8492391
ruthleee <seojinruthy@gmail.com>
2020-07-25 23:51:11 +0000

    parseHub works

--- app.py
+++ app.py
@@ -2,19 +2,20 @@
 # -- Import section --
 # AIzaSyCSsfexfhI7I3r-MXUuSmD3_0oVRNLjs1s
 from flask import Flask
-<<<<<<< HEAD
-
+# from flask import render_template
+# from flask import request
+from googleapiclient import discovery
 # from flask import render_template
 # from flask import request
 from googleapiclient import discovery
 
-=======
-# from flask import render_template
-# from flask import request
-from googleapiclient import discovery
->>>>>>> 40844f5ad654e89daaab99465b040581d3be92ef
 from flask import render_template
 from flask import request
+import requests
+import urllib.request
+import re
+from urllib.request import Request, urlopen
+
 
 
 
@@ -65,6 +66,36 @@ def youtube():
             print(each_item['snippet']['description'])
             print(each_item['snippet']['thumbnails']['high']['url'])
     return "hello world"
+@app.route('/parsehub')
+def parsehub():
+    params = {
+        "api_key": "tAgMZD_gGfMN",
+        "format": "json",
+        "project_token": "tTunoV0JjdT4",
+        "start_url": "https://directory.goodonyou.eco/brand/reformation",
+        "send_email": "1"
+
+    }
+    r = requests.post("https://www.parsehub.com/api/v2/projects/tTunoV0JjdT4/run", data=params)
+    print(r.json["run_token"])
+    # p = requests.get('https://www.parsehub.com/api/v2/projects/tTunoV0JjdT4/last_ready_run/data', params=params)
+    # print(p.text)
+    return("hello)")
+@app.route('/webS')
+def webS():
+    req = Request('http://pythonprogramming.net/parse-website-using-regular-expressions-urllib/', headers={'User-Agent': 'Mozilla/5.0'})
+    respData = urlopen(req).read()
+    # url = 'https://directory.goodonyou.eco/brand/white-house-black-market'
+
+    # req = urllib.request.Request(url)
+    # resp = urllib.request.urlopen(req)
+    # respData = resp.read()
+    rated = re.findall(r'<p>(.*?)</p>',str(respData))
+    # <span class="StyledText-sc-1sadyjn-0 bBUTWf">Rated: Not Good Enough</span>
+    print(rated)
+    # for eachR in rated:
+    #     print(eachR)
+    return("hellur")
 
 
 
