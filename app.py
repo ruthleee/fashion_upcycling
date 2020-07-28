84fb55143633ccb116ae46a72da878785cc4386b
savsrin <savsrin@gmail.com>
2020-07-27 22:43:25 -0700

    most recent app.py

--- app.py
+++ app.py
@@ -2,11 +2,22 @@
 # -- Import section --
 # AIzaSyCSsfexfhI7I3r-MXUuSmD3_0oVRNLjs1s
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from googleapiclient import discovery
 from googleapiclient import discovery
 from flask import render_template
 from flask import request
+import requests
+import urllib.request
+import re
+from urllib.request import Request, urlopen
+import time
+from model import youtube_search
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+from flask_pymongo import PyMongo
+from flask import session
+import bcrypt
+
 
 
 
@@ -18,39 +29,116 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    api_key = "AIzaSyC6rqXcj4ZRxEBW_t-mPzr_3G4ei25HtO8"
-    youtube = discovery.build('youtube', 'v3', developerKey=api_key)
-    # req = youtube.search().list(q='machine learning tutorial', part='snippet', type='video', maxResults=50, pageToken=None)
-    # res = req.execute()
-    MAX_COUNT = 10
-    nextPageToken =  None
-    #items[query number]['id']['videoId']
-    #items[query number]['snippet']['title]
-    #items[query number]['snippet']['description']
-    #items[query number]['snippet']['description']['thumbnails']['high']['url'] (['width'], ['height'])
+    return render_template("index.html")
+@app.route('/upcycleSearch', methods=["GET", "POST"])
+def upcycleSearch():
+    
+    return render_template("upcycleSearch.html")
+@app.route('/upcycleResults', methods=["GET", "POST"])
+def upcycleResults():
+    if request.method == "POST":
+        user_garment = request.form["garment"]
+        user_brand = request.form["brand"]
+        user_price = request.form["price"]
+        return render_template('upcycleResults.html', user_garment=user_garment, user_brand=user_brand, user_price=user_price)
+    else:
+        return "Error. Nothing submitted. Please go back to the <a href='/upcycleSearch'>Upcycle Page</a>"
+@app.route('/parsehub')
+def parsehub():
+    params = {
+        "api_key": "tAgMZD_gGfMN",
+        "format": "json",
+        "project_token": "tTunoV0JjdT4",
+        "start_url": "https://directory.goodonyou.eco/brand/the-r-collective",
+        "send_email": "1"
+    }
+    r = requests.post("https://www.parsehub.com/api/v2/projects/tTunoV0JjdT4/run", data=params)
+    run_token = r.json()["run_token"]
+    p = requests.get('https://www.parsehub.com/api/v2/runs/' + run_token, params=params)
+    for i in range(10): 
+        time.sleep(1)
+        p = requests.get('https://www.parsehub.com/api/v2/runs/' + run_token, params=params)
+        if p.json()["data_ready"] == 1: 
+            break
+    
+    print(p.text)
+    final_data = requests.get("https://www.parsehub.com/api/v2/projects/tTunoV0JjdT4/last_ready_run/data", params=params)
+    print(final_data.text)
+    return("hellour")
 
-    search_by = 'DIY crop hoodie'
-    for i in range(1):
-        req = youtube.search().list(q=search_by, part='snippet', type='video', maxResults=MAX_COUNT, pageToken=nextPageToken)
-        res = req.execute()
-        nextPageToken = res['nextPageToken']
-        items = res['items']
-        if res['nextPageToken'] == None:
-            break; # exit from the loop
-        # print(items[0])
-        # print(items[0]['id']['videoId'])
-        # print(items[0]['snippet']['title'])
-        # print(items[0]['snippet']['description'])
-        # print(items[0]['snippet']['thumbnails']['high']['url'])
 
-        for each_item in items:
-            #store in DB or file or print the same.
-            # print (each_item['id']['videoId'])
-            print(each_item['id']['videoId'])
-            print(each_item['snippet']['title'])
-            print(each_item['snippet']['description'])
-            print(each_item['snippet']['thumbnails']['high']['url'])
-    return "hello world"
+@app.route('/parse_url')
+def parse_url(): 
+    url = 'https://www2.hm.com/en_us/productpage.0876657002.html'
+    headers = {}
+    headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+    req = Request(url=url, headers=headers) 
+    html = urlopen(req).read() 
+    soup = BeautifulSoup(html, 'lxml')
+    type(soup)
+    title = soup.title
+    print(title)
+    head_elem = soup.find('h1')
+    print(head_elem.text)
+    def contains_price(s3):
+     return ("$" in s3)
+    print(soup.find_all(string=contains_price))
+    return ("hello")
+    # print(soup.find_all('html'))
+    # text = soup.get_text()
+    # print(text.strip('\n'))
+    # prices = soup.find_all('span')
+    # print(prices)
+    # Print out the text
+    # text = soup.get_text()
+    # print(soup.text)
+@app.route("/parse_rating")
+def parse_rating():
+    params = {
+        "api_key": "tAgMZD_gGfMN",
+        "format": "json",
+        "project_token": "tTunoV0JjdT4",
+        "start_url": "https://directory.goodonyou.eco/brand/the-r-collective",
+        "send_email": "1"
+    }
+    r = requests.post("https://www.parsehub.com/api/v2/projects/tTunoV0JjdT4/run", data=params)
+    run_token = r.json()["run_token"]
+    p = requests.get('https://www.parsehub.com/api/v2/runs/' + run_token, params=params)
+    for i in range(10): 
+        time.sleep(1)
+        p = requests.get('https://www.parsehub.com/api/v2/runs/' + run_token, params=params)
+        if p.json()["data_ready"] == 1: 
+            break
+    
+    print(p.text)
+    final_data = requests.get("https://www.parsehub.com/api/v2/projects/tTunoV0JjdT4/last_ready_run/data", params=params)
+    print(final_data.text)
+    return("hellour")
 
+app.secret_key = "k2u3gogsdboqasd34"
+# name of database
+app.config['MONGO_DBNAME'] = 'database'
 
+# URI of database
+app.config['MONGO_URI'] = 'mongodb+srv://admin:OmSyXfRK8jG98xVq@couture.zvxpp.mongodb.net/database?retryWrites=true&w=majority'
 
+mongo = PyMongo(app)
+@app.route("/loginsignup", methods=["GET", "POST"])
+def loginsignup():
+    session.clear()
+    if request.method == "GET":
+        return render_template('login_signup.html')
+    else:
+        username = request.form["username"]
+        password = request.form["password"]
+        collection = mongo.db.users
+        user = list(collection.find({"username":username}))
+        if len(user) == 0:
+            collection.insert_one({"username": username, "password": str(bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()), 'utf-8')})
+            session["username"] = username
+            return("Welcome as a new user")
+        elif bcrypt.hashpw(password.encode('utf-8'), user[0]['password'].encode('utf-8')) == user[0]['password'].encode('utf-8'):
+            session["username"] = username
+            return"Welcome back! Go to <a href='/index'>home</a>."
+        else:
+            return "Error"
---
+++ model.py
@@ -0,0 +1,23 @@
+from googleapiclient import discovery
+def search_youtube(item_name, num_queries=10):
+    api_key = "AIzaSyC6rqXcj4ZRxEBW_t-mPzr_3G4ei25HtO8"
+    youtube = discovery.build('youtube', 'v3', developerKey=api_key)
+    queries = [" DIY", "  Thrift Flip", " Upcycle Tutorial"]
+    #num_results = 0 
+    search_results = {}
+    for query in queries: 
+        search_by = item_name + query
+        request = youtube.search().list(q=search_by, part='snippet', type='video', maxResults=num_queries, pageToken=None)
+        result = request.execute() 
+        #num_results += result['pageInfo.totalResults']
+        items = result['items']
+        for each_item in items: 
+            search_results[each_item['snippet']['title']] = {"video_url": "https://www.youtube.com/watch?v=" + each_item['id']['videoId'], 
+                                                            "description": each_item['snippet']['description'], 
+                                                            "thumbnail_url": each_item['snippet']['thumbnails']['high']['url']}
+        if len(search_results) > 0 and query == "  Thrift Flip": 
+            break
+    if len(search_results) == 0: 
+        return "No upcycling tutorials were found for '" +  item-name + ".' Please try searching with a different item name."
+    else: 
+        return search_results
