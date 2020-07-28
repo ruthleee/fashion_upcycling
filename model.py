b634ddca79d546ab6791f507ccc1d22889126ec9
ruthleee <seojinruthy@gmail.com>
2020-07-28 13:38:03 +0000

    mod

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from googleapiclient import discovery
 def search_youtube(item_name, num_queries=10):
-    api_key = "AIzaSyC6rqXcj4ZRxEBW_t-mPzr_3G4ei25HtO8"
+    api_key = "AIzaSyDChP61rSI95mLnjomoCBXKpnD-YrnZKO4"
     youtube = discovery.build('youtube', 'v3', developerKey=api_key)
     queries = [" DIY", "  Thrift Flip", " Upcycle Tutorial"]
     #num_results = 0 
@@ -17,6 +17,7 @@ def search_youtube(item_name, num_queries=10):
                                                             "thumbnail_url": each_item['snippet']['thumbnails']['high']['url']}
         if len(search_results) > 0 and query == "  Thrift Flip": 
             break
+        print(search_results)
     if len(search_results) == 0: 
         return "No upcycling tutorials were found for '" +  item-name + ".' Please try searching with a different item name."
     else: 
